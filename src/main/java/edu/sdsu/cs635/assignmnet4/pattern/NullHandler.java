42a609745a44c95b9511c99d986e750d80161f1e
HRSHDG8 <hrshdg8@gmail.com>
2021-12-13 08:33:41 -0800

    final code

--- src/main/java/edu/sdsu/cs635/assignmnet4/pattern/NullHandler.java
+++ src/main/java/edu/sdsu/cs635/assignmnet4/pattern/NullHandler.java
@@ -6,6 +6,7 @@ import edu.sdsu.cs635.assignmnet4.handler.Handler;
  * Null Object Pattern implementation of {@link Handler} interface
  * Used at end of request chain to avoid null check before invoking handle method.
  * Note : This class cannot extend {@link AbstractPatternMatchHandler} since it does not want default implementation of setNext()
+ *        and {@link NullHandler} is instantiated in Constructor of {@link AbstractPatternMatchHandler} which would cause stackoverflow
  */
 public class NullHandler implements Handler<PatternMatchRequest> {
 
